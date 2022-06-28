84e2ac7cf28cd2708f55acaf23007df32fe361a4
SangHun <shzero211@naver.com>
2022-06-28 17:13:36 +0900

    블로그 상세 페이지

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Layout from "../components/Layout";
+import Layout from "../../components/Layout";
 import { useStaticQuery,graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 // markup
