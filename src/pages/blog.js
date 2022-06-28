6f45a846ed1a7c6d46060b7b2d8aa14c459912c6
SangHun <shzero211@naver.com>
2022-06-28 16:48:37 +0900

    블로그 내용 표시

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,36 @@
 import * as React from "react"
 import Layout from "../components/Layout";
 import { useStaticQuery,graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 // markup
 const BlogPage = () => {
   const data=useStaticQuery(graphql`
   query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          
+        }
+        id
+        body
       }
     }
-  }
-  `);
+  }`);
   return (
       
       <Layout pageTitle="My Blog Posts">
         <p>My cool posts will go in here</p>
         <ul>
-          {data.allFile.nodes.map((node)=>(
-            <li key={node.name}>{node.name}</li>
+          {data.allMdx.nodes.map((node)=>(
+            <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <h2>{node.frontmatter.date}</h2>
+           <MDXRenderer>
+            {node.body}
+           </MDXRenderer>
+           </article>
           ))}
         </ul>
       </Layout>
