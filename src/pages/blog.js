71c780b96fedf54d3fc6f870c131323df4d85e4b
SangHun <shzero211@naver.com>
2022-06-28 16:28:54 +0900

    blog 포스트 리스트 출력

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,26 @@
-import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react"
 import Layout from "../components/Layout";
+import { useStaticQuery,graphql } from "gatsby";
 // markup
 const BlogPage = () => {
+  const data=useStaticQuery(graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+  `);
   return (
       
       <Layout pageTitle="My Blog Posts">
         <p>My cool posts will go in here</p>
+        <ul>
+          {data.allFile.nodes.map((node)=>(
+            <li key={node.name}>{node.name}</li>
+          ))}
+        </ul>
       </Layout>
   ); 
 };
